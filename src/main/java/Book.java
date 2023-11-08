c4a787fbe3e5bb4b694f12285639f5b48bfa8e4e
Tho-gaKo <thomas-gabriel.korak@coop.ch>
2023-11-08 14:43:42 +0100

    Initial commit

---
+++ src/main/java/Book.java
@@ -0,0 +1,32 @@
+public class Book {
+
+    private String title;
+
+    private String author;
+
+    private int isbn;
+
+
+    Book() {
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public void setIsbn(int isbn) {
+        this.isbn = isbn;
+    }
+
+
+
+
+
+
+    }
+
+
+}
---
+++ src/main/java/Bookobject.java
@@ -0,0 +1,5 @@
+public class Bookobject {
+    public static void main(String[] args) {
+
+    }
+}
