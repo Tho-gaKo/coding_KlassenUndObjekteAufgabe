ad215aea5a9812e7fbbbdfbfed337e776a8c0a01
Tho-gaKo <thomas-gabriel.korak@coop.ch>
2023-11-08 16:26:28 +0100

    bookArr

---
+++ src/main/java/Library.java
@@ -0,0 +1,16 @@
+public class Library {
+
+    private Book[] bookArr;
+
+    public void setBookArr(Book[] books) {
+        this.bookArr = books;
+    }
+    public Book[] getBookArr() {
+        return bookArr;
+    }
+    public String toString() {
+        return
+    }
+
+
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,29 @@
+import java.util.Arrays;
+
 public class Main {
 
     public static void main(String[] args) {
 
+        Book book3 = new Book();
+        book3.setTitle("Guidebook for Hades");
+        book3.setAuthor("Supergiant Games");
+        book3.setIsbn(814519);
+        Book book2 = new Book();
+        book2.setTitle("Guidebook for SKUL");
+        book2.setAuthor("SOUTHPAW GAMES");
+        book2.setIsbn(19102211);
         Book book = new Book();
         book.setTitle("Guidebook for Dead Cells");
         book.setAuthor("Motion Twin");
         book.setIsbn(451435121);
         System.out.println(book);
+
+        Library library = new Library();
+        library.setBookArr(new Book[]{book, book2, book3});
+        library.getBookArr();
+        System.out.println(Arrays.toString(library.getBookArr()));
+        System.out.println(library);
     }
+
+
 }
